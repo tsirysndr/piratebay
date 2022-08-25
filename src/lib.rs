c0f03601327222b7b39c26a82cdcc8845cc31ebb
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-08-25 10:23:19 +0300

    update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//! # piratebay crate
+//!
+//! <p>
+//! <a href="https://crates.io/crates/piratebay" target="_blank">
+//!  <img src="https://img.shields.io/crates/v/piratebay.svg" />
+//! </a>
+//! <a href="https://docs.rs/piratebay" target="_blank">
+//!  <img src="https://docs.rs/piratebay/badge.svg" />
+//! </a>
+//! <a href="LICENSE" target="_blank">
+//!  <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-blue.svg" />
+//! </a>
+//! </p>
+//!
+//! PirateBay is a Rust library for the [Pirate Bay](https://thepiratebay.org)
+
 pub mod constants;
 pub mod formater;
 pub mod pirateclient;
