b03444a93d7f258a78a70e1ebe03b7227a32b83f
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-02 18:30:17 +0000

    fix: update piratebay API URL

--- src/types.rs
+++ src/types.rs
@@ -10,7 +10,7 @@ pub struct Torrent {
     #[tabled(skip)]
     pub descr: Option<String>,
     #[tabled(skip)]
-    pub download_count: String,
+    pub download_count: Option<String>,
     pub id: String,
     #[serde(rename = "info_hash")]
     #[tabled(skip)]
@@ -22,6 +22,33 @@ pub struct Torrent {
     pub num_files: String,
     pub seeders: String,
     pub size: String,
+    pub status: String,
+    pub username: String,
+    #[tabled(skip)]
+    pub magnet: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Tabled)]
+#[serde(rename_all = "camelCase")]
+pub struct TorrentInfo {
+    pub added: i64,
+    #[tabled(skip)]
+    pub category: u32,
+    #[tabled(skip)]
+    pub descr: Option<String>,
+    #[tabled(skip)]
+    pub download_count: Option<String>,
+    pub id: u32,
+    #[serde(rename = "info_hash")]
+    #[tabled(skip)]
+    pub info_hash: String,
+    pub leechers: u32,
+    pub name: String,
+    #[serde(rename = "num_files")]
+    #[tabled(skip)]
+    pub num_files: u32,
+    pub seeders: u32,
+    pub size: u64,
     pub status: String,
     pub username: String,
     #[tabled(skip)]
